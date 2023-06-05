7b156d1c6abd50f95938d5f6a274411783d5648c
Adopt-Pet <celine.nvalencia@gmail.com>
2023-06-05 22:26:29 +0800

    recode menu section

--- app/components/Menu/MenuSection.tsx
+++ app/components/Menu/MenuSection.tsx
@@ -1,163 +1,69 @@
 import React from "react";
 
 function MenuSection() {
+  const Menu = [
+    {
+      title: "Coffee",
+      category: "Hot Drinks",
+      price: "$16.00",
+      imageUrl:
+        "https://images.unsplash.com/photo-1544787219-7f47ccb76574?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=721&q=80",
+    },
+    {
+      title: "Chocolate Drink",
+      category: "Hot Drinks",
+      price: "$16.00",
+      imageUrl:
+        "https://images.unsplash.com/photo-1544787219-7f47ccb76574?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=721&q=80",
+    },
+    {
+      title: "Chocolate Drink",
+      category: "Hot Drinks",
+      price: "$16.00",
+      imageUrl:
+        "https://images.unsplash.com/photo-1572286258217-40142c1c6a70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80",
+    },
+    {
+      title: "Chocolate Drink",
+      category: "Hot Drinks",
+      price: "$16.00",
+      imageUrl:
+        "https://images.unsplash.com/photo-1572286258217-40142c1c6a70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80",
+    },
+  ];
   return (
-    <section className="text-gray-600 body-font">
+    <section className="text-gray-600 body-font lg:h-screen">
       <div className="container px-5 py-24 mx-auto flex flex-wrap justify-center ">
         <div>
           <h1 className="text-3xl font-extrabold sm:text-5xl text-primary ">
-            The Coffee Blog
+            The Coffee Menu
           </h1>
           <p className="mt-4 max-w-lg sm:text-xl/relaxed text-primary font-light">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
             illo tenetur fuga ducimus numquam ea!
           </p>
         </div>
-        <div className="flex flex-wrap -m-4">
-          <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img
-                alt="ecommerce"
-                className="object-cover object-center w-full h-full block"
-                src="https://images.unsplash.com/photo-1559496417-e7f25cb247f3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80"
-              />
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                CATEGORY
-              </h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">
-                The Catalyzer
-              </h2>
-              <p className="mt-1">$16.00</p>
+        <div className="flex flex-wrap w-11/12">
+          {Menu.map((menu) => (
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full" key={menu.title}>
+              <a className="block relative  rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-48 block"
+                  src={menu.imageUrl}
+                />
+              </a>
+              <div className="mt-4 ">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  {menu.category}
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  {menu.title}
+                </h2>
+                <p className="mt-1">{menu.price}</p>
+              </div>
             </div>
-          </div>
-          <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img
-                alt="ecommerce"
-                className="object-cover object-center w-full h-full block"
-                src="https://images.unsplash.com/photo-1497636577773-f1231844b336?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
-              />
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                CATEGORY
-              </h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">
-                Shooting Stars
-              </h2>
-              <p className="mt-1">$21.15</p>
-            </div>
-          </div>
-          <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img
-                alt="ecommerce"
-                className="object-cover object-center w-full h-full block"
-                src="https://images.unsplash.com/photo-1515701412241-6cd81fd1fd89?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
-              />
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                CATEGORY
-              </h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">
-                Neptune
-              </h2>
-              <p className="mt-1">$12.00</p>
-            </div>
-          </div>
-          <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img
-                alt="ecommerce"
-                className="object-cover object-center w-full h-full block"
-                src="https://images.unsplash.com/photo-1544787219-7f47ccb76574?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=721&q=80"
-              />
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                CATEGORY
-              </h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">
-                The 400 Blows
-              </h2>
-              <p className="mt-1">$18.40</p>
-            </div>
-          </div>
-          <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img
-                alt="ecommerce"
-                className="object-cover object-center w-full h-full block"
-                src="https://images.unsplash.com/photo-1589985902809-39d25db22101?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
-              />
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                CATEGORY
-              </h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">
-                The Catalyzer
-              </h2>
-              <p className="mt-1">$16.00</p>
-            </div>
-          </div>
-          <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img
-                alt="ecommerce"
-                className="object-cover object-center w-full h-full block"
-                src="https://images.unsplash.com/photo-1615486780246-76e6bb33e8b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80"
-              />
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                CATEGORY
-              </h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">
-                Shooting Stars
-              </h2>
-              <p className="mt-1">$21.15</p>
-            </div>
-          </div>
-          <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img
-                alt="ecommerce"
-                className="object-cover object-center w-full h-full block"
-                src="https://images.unsplash.com/photo-1515823064-d6e0c04616a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80"
-              />
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                CATEGORY
-              </h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">
-                Neptune
-              </h2>
-              <p className="mt-1">$12.00</p>
-            </div>
-          </div>
-          <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img
-                alt="ecommerce"
-                className="object-cover object-center w-full h-full block"
-                src="https://images.unsplash.com/photo-1521302080334-4bebac2763a6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80"
-              />
-            </a>
-            <div className="mt-4">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                CATEGORY
-              </h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">
-                The 400 Blows
-              </h2>
-              <p className="mt-1">$18.40</p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
--- app/components/blog/BlogSection.tsx
+++ app/components/blog/BlogSection.tsx
@@ -1,6 +1,29 @@
 import React from "react";
 
 function BlogSection() {
+  const blogDetails = [
+    {
+      title: " Finding the Journey to Mordor",
+      ImageUrl:
+        "https://images.unsplash.com/photo-1507914464562-6ff4ac29692f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Recusandae dolores, possimus pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius atque dignissimos. Molestias explicabo corporis voluptatem?",
+    },
+    {
+      title: " Finding the Journey to Mordor",
+      ImageUrl:
+        "https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNhZmV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Recusandae dolores, possimus pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius atque dignissimos. Molestias explicabo corporis voluptatem?",
+    },
+    {
+      title: " Finding the Journey to Mordor",
+      ImageUrl:
+        "https://images.unsplash.com/photo-1507915135761-41a0a222c709?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Recusandae dolores, possimus pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius atque dignissimos. Molestias explicabo corporis voluptatem?",
+    },
+  ];
   return (
     <div className="flex flex-wrap justify-center content-center w-5/6 m-auto lg:h-screen items-center text-center">
       <div>
@@ -13,78 +36,27 @@ function BlogSection() {
         </p>
       </div>
       <div className="flex flex-wrap  justify-center items-center gap-4  ">
-        <article className=" lg:w-3/12 md:w-2/6">
-          <img
-            alt="Lava"
-            src="https://images.unsplash.com/photo-1507914464562-6ff4ac29692f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
-            className="h-56 lg:h-80 w-full rounded-xl object-cover shadow-xl transition group-hover:grayscale-[50%]"
-          />
+        {blogDetails.map((blogDetails) => (
+          <article className=" lg:w-3/12 md:w-2/6" key={blogDetails.title}>
+            <img
+              alt="Lava"
+              src={blogDetails.ImageUrl}
+              className="h-56 lg:h-80 w-full rounded-xl object-cover shadow-xl transition group-hover:grayscale-[50%]"
+            />
 
-          <div className="p-4">
-            <a href="#">
-              <h3 className="text-lg font-medium text-gray-900">
-                Finding the Journey to Mordor
-              </h3>
-            </a>
+            <div className="p-4">
+              <a href="#">
+                <h3 className="text-lg font-medium text-gray-900">
+                  {blogDetails.title}
+                </h3>
+              </a>
 
-            <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Recusandae dolores, possimus pariatur animi temporibus nesciunt
-              praesentium dolore sed nulla ipsum eveniet corporis quidem,
-              mollitia itaque minus soluta, voluptates neque explicabo tempora
-              nisi culpa eius atque dignissimos. Molestias explicabo corporis
-              voluptatem?
-            </p>
-          </div>
-        </article>
-        <article className=" lg:w-3/12 md:w-2/6">
-          <img
-            alt="Lava"
-            src="https://images.unsplash.com/photo-1551462577-9aaf3ff5d927?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1476&q=80"
-            className="h-56 lg:h-80 w-full rounded-xl object-cover shadow-xl transition group-hover:grayscale-[50%]"
-          />
-
-          <div className="p-4">
-            <a href="#">
-              <h3 className="text-lg font-medium text-gray-900">
-                Finding the Journey to Mordor
-              </h3>
-            </a>
-
-            <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Recusandae dolores, possimus pariatur animi temporibus nesciunt
-              praesentium dolore sed nulla ipsum eveniet corporis quidem,
-              mollitia itaque minus soluta, voluptates neque explicabo tempora
-              nisi culpa eius atque dignissimos. Molestias explicabo corporis
-              voluptatem?
-            </p>
-          </div>
-        </article>
-        <article className=" lg:w-3/12 md:w-2/6">
-          <img
-            alt="Lava"
-            src="https://images.unsplash.com/photo-1507915135761-41a0a222c709?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80"
-            className="h-56 lg:h-80 w-full rounded-xl object-cover shadow-xl transition group-hover:grayscale-[50%]"
-          />
-
-          <div className="p-4">
-            <a href="#">
-              <h3 className="text-lg font-medium text-gray-900">
-                Finding the Journey to Mordor
-              </h3>
-            </a>
-
-            <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Recusandae dolores, possimus pariatur animi temporibus nesciunt
-              praesentium dolore sed nulla ipsum eveniet corporis quidem,
-              mollitia itaque minus soluta, voluptates neque explicabo tempora
-              nisi culpa eius atque dignissimos. Molestias explicabo corporis
-              voluptatem?
-            </p>
-          </div>
-        </article>
+              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+                {blogDetails.description}
+              </p>
+            </div>
+          </article>
+        ))}
       </div>
     </div>
   );
