f5a12d16eddea4c691deb4684a1a82c507753d8b
Adopt-Pet <celine.nvalencia@gmail.com>
2023-06-06 23:28:13 +0800

    Navbar Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,25 @@
-import './globals.css'
-import { Inter } from 'next/font/google';
-import { Html, Head, Main, NextScript } from "next/document";
+import "./globals.css";
+import { Inter } from "next/font/google";
 import Script from "next/script";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Cafe',
-  description: 'Cafe',
-}
+  title: "Cafe",
+  description: "Cafe",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Script src="./assets/vendor/preline/dist/preline.js"></Script>
+      </body>
     </html>
-  )
+  );
 }
