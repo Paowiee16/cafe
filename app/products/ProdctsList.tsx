8e8037f926beb54842d5c7d7844d747464d9c23e
Adopt-Pet <celine.nvalencia@gmail.com>
2023-06-28 04:54:43 +0800

    change fonts and update menu page

--- app/products/ProdctsList.tsx
+++ app/products/ProdctsList.tsx
@@ -4,25 +4,37 @@ const ProductList = () => {
   const { data, isPending, error } = useFetch(" http://localhost:8000/coffee");
   console.log(data);
   return (
-    <div className="flex flex-wrap justify-center content-center w-5/6 m-auto lg:p-10 items-center text-center">
-      <div className="m-auto">
+    <div className="flex flex-wrap justify-center content-center w-5/6 m-auto lg:p-10 items-center text-center container">
+      <div className="m-auto w-90">
         <h1 className="text-3xl font-extrabold sm:text-5xl text-primary ">
           The Menu
         </h1>
-        <p className="mt-4 max-w-lg sm:text-xl/relaxed text-primary font-light">
+        <p className="mt-4  sm:text-xl/relaxed text-primary font-light">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
           illo tenetur fuga ducimus numquam ea!
         </p>
       </div>
-      <div className="m-auto  grid-cols-3 grid">
+      <div className="flex flex-wrap w-11/12 text-left">
+        {isPending && <div className=" pending">Loading...</div>}
         {data?.map((coffee: any) => (
-          <div
-            key={coffee.id}
-            className="grid grid-cols-3 justify-center items-center flex-row gap-2 "
-          >
-            <img src={coffee.img} className="w-12 h-12  rounded-full" />
-            <p>{coffee.name}</p>
-            <p> &#8369; {coffee.price}</p>
+          <div className="lg:w-1/4 md:w-1/2 p-4 w-full" key={coffee.id}>
+            <a className="block relative  rounded overflow-hidden">
+              <img
+                alt="ecommerce"
+                className="object-cover object-center w-full h-48 block"
+                src={coffee.img}
+                loading="lazy"
+              />
+            </a>
+            <div className="mt-4 ">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                {coffee.category} Drink
+              </h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">
+                {coffee.name}
+              </h2>
+              <p className="mt-1">${coffee.price}</p>
+            </div>
           </div>
         ))}
       </div>
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -5,7 +5,7 @@ import ProductList from "./ProdctsList";
 
 const Products = () => {
   const [Details, setDetails] = useState({
-    title: "About",
+    title: "Menu",
     BgImage:
       "https://images.unsplash.com/photo-1481833761820-0509d3217039?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
   });
